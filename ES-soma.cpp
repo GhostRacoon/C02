e5d1900e752a8b12af52ff2646b0ac706a9d4c34
Gustavo Bellini <161270960+GhostRacoon@users.noreply.github.com>
2024-02-29 22:04:56 -0300

    Add files via upload

---
+++ ES-soma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+	
+	int main(){
+		   
+		   int A, B;
+		   int x;
+		   
+		   cin >> A >> B;
+		   
+		   x = A + B;
+		   
+		   cout << "X = " << x;
+		   	
+}
